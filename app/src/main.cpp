11414f56b1a870b352c64b64441ecc77806f6524
Carlos Pantelides <carlos.pantelides@gmail.com>
2026-05-01 15:29:04 -0300

    added app.overlay, kconfig and fixed main.src

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -2,10 +2,10 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
-#define SLEEP_TIME_MS 1000
+#define SLEEP_TIME_MS CONFIG_APP_HEARTBEAT_PERIOD_MS
 
-/* The devicetree node identifier for the "led0" alias. */
-#define LED_NODE DT_ALIAS(led0)
+/* The devicetree node identifier for the "app_led" alias. */
+#define LED_NODE DT_ALIAS(app_led)
 
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
 
